6dbd455a515d4403cd3ab8aca9f8f3abf06f24e3
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-06 14:55:52 +0200

    Basic Login Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,35 @@
 const Login = () => {
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        console.log("🧚 ~ formData - email", formData.get('email'));
+        
+    }
+
     return (
-        <p>LOGIN PAGE</p>
+        <section>
+            <form id="login-form" onSubmit={onLogin}>
+                <fieldset>
+                    <label>LOGIN</label>
+                    <p>
+                        <label for="email">Email</label>
+                        <span>
+                            <input type="text" name="email" id="email" placeholder="Email" />
+                        </span>
+                    </p>
+                    <p>
+                        <label for="password">Password</label>
+                        <span>
+                            <input type="password" name="password" id="password" placeholder="Password" />
+                        </span>
+                    </p>
+                    <input class="button" type="submit" value="Login" />
+
+                </fieldset>
+            </form>
+
+        </section>
     );
 }
 
