c727bb9428539c3de986e1537451dd8172055b0f
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-06 20:59:52 +0200

    Removed nested routed from Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,18 +5,12 @@ const Dashboard = () => {
     return (
         <section id="dashboard-page" className="dashboard">
             <h1>Dashboard</h1>
-            <nav>
-                <Link to="genres">Genres</Link>
 
-            </nav>
-            <section>
-                <Routes>
-                    <Route path="/" element={<BookList />} />
-                    <Route path="/genres" element="GENRES......" />
-
-                </Routes>
-            </section>
+            <Routes>
+                <Route path="/" element={<BookList />} />
+            </Routes>
         </section>
+
     );
 }
 
