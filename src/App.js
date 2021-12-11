b6f4c019a1c819d63a19080231faef26c99761d0
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-12 00:44:42 +0200

    added persistance of data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 
 import { AuthContext } from './contexts/AuthContext';
+import useLocalStorage from './hooks/useLocalStorage';
 import CreateBook from './components/CreateBook';
 import Dashboard from './components/Dashboard';
 import Details from './components/Details';
@@ -11,14 +12,13 @@ import Login from './components/Login';
 import OwnedBooks from './components/OwnedBooks';
 import Register from './components/Register';
 import WishList from './components/WishList';
-import { isAuthenticated } from './services/authService';
 import Logout from './components/Logout';
 
 
 
 function App() {
 
-  const [user, setUser] = useState({
+  const [user, setUser] = useLocalStorage('user', {
     accessToken: '',
     email: '',
     _id: ''
