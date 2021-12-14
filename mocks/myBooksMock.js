26b952317a9e27a09c241d22a28a7996b6702493
YuliiaAlieks <yalieksieienko@gmail.com>
2021-12-14 20:50:23 +0200

    Correctedmy books  mock

--- mocks/myBooksMock.js
+++ mocks/myBooksMock.js
@@ -1,15 +1,13 @@
-let myBooks = [
+const myBooks = [
     {
         "author": "J. R. R. Tolkien",
         "description": "In ancient times the Rings of Power were crafted by the Elven-smiths, and Sauron, the Dark Lord, forged the One Ring, filling it with his own power so that he could rule all others. But the One Ring was taken from him, and though he sought it throughout Middle-earth, it remained lost to him. After many ages it fell by chance into the hands of the hobbit Bilbo Baggins.",
-        'genre': "other",
-        'imageUrl': "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg",
+        "genre": "other",
+        "imageUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg",
         "likes": [],
-        'recommendedUrl': "https://www.goodreads.com/book/show/33.The_Lord_of_the_Rings",
-        'title': "The Lord of the Rings",
-        'year': "1954",
-        "_createdOn": 1639504555528,
-        "_id": "44c71a79-2ff2-469a-9164-47f66245d4e7",
+        "recommendedUrl": "https://www.goodreads.com/book/show/33.The_Lord_of_the_Rings",
+        "title": "The Lord of the Rings",
+        "year": "1954",
         "_ownerId": "35c62d76-8152-4626-8712-eeb96381bea8"
     },
     {
@@ -21,8 +19,6 @@ let myBooks = [
         "recommendedUrl": "https://www.goodreads.com/book/show/72193.Harry_Potter_and_the_Philosopher_s_Stone?from_search=true&from_srp=true&qid=LorGeS3hii&rank=1",
         "title": "Harry Potter and the Philosopher's Stone",
         "year": "1997",
-        "_createdOn": 1639505039614,
-        "_id": "818afde9-3bb2-4217-b7fd-258ac1fa9007",
         "_ownerId": "35c62d76-8152-4626-8712-eeb96381bea8"
     }
 
